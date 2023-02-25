8d4f6b867559dbbcf8fb1f959e060108d333bf98
Pavel Polkanov <63466418+PaulPolkanov@users.noreply.github.com>
2023-02-26 02:51:29 +0300

    Bugfix

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -21,14 +21,14 @@ class IndexController extends Controller
         //Данный валидатор проверяет наличие и правильность принимаемой даты
         $validator = DateValidator::dateCheck($request);
         if($validator->fails()){
-            //Если данные не соответсвуют, то редиректим на главную страницу
+            //Если данные не соответствуют, то редиректим на главную страницу
             return redirect()->with('home');
         }
         $tamplate = $this->template;
         $date = $request->date;
         //Получаем предзаказы по выбранной дате
         $orders = Order::where('date', $date)->get();
-        //Отправляем дату в Helper который расчитает остаток на складе и цену товаров
+        //Отправляем дату в Helper, который рассчитает  остаток на складе и цену товаров
         $storage = Calculation::countOnStorage($date);
         $prices = Calculation::calculPrice($date);
         return view('pages.main', compact('tamplate', 'date', 'orders', 'storage', 'prices'));
